bde67c317c2cc52228724c921785e2c5532b4e07
khanh10101998 <khanh10101998@gmail.com>
2019-05-23 09:44:10 +0700

    update test

--- test.sh
+++ test.sh
@@ -6,13 +6,11 @@ while IFS= read -r f1; do
     IFS=''
     while IFS= read -r a || [ -n "$a" ];
         do
-            # if [[ $a == *"it("* ]]; then
-                firstLine=${arrString[0]}
-            if [ "$firstLine" == "it('LINE" ]; then
-                lineNumber=$(sed 's/[:]//g'<<<"${array[0]}")
                 IFS=' ' read -r -a arrString <<< "$a"
+                firstLine=${arrString[0]}
+            # if [[ $a == *"it("* ]]; then
+            if [ "$firstLine" == "it('LINE" ]; then
                 numLineDelete=${arrString[1]}
-
                 DE=$(sed "s/$numLineDelete/$numLine/g"<<<"${a}")
                 echo $DE
             elif [ "$firstLine" == "//" ]; then
