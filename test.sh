1c729b3d79f5ae074a397fe0470b34f67de606fc
khanh10101998 <khanh10101998@gmail.com>
2019-05-23 14:13:02 +0700

    update

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+# chmod 777 ./file
 cd e2e
 while IFS= read -r f1; do
     path=${f1:2}
@@ -7,15 +8,25 @@ while IFS= read -r f1; do
         do
                 IFS=' ' read -r -a arrString <<< "$a"
                 firstLine=${arrString[0]}
-            if [ "$firstLine" == "it('LINE" -o "$firstLine" == 'it("LINE' ]; then
-                numLineDelete=${arrString[1]}
-                DE=$(sed "s/$numLineDelete/$numLine/g"<<<"${a}")
-                echo $DE
+                itString="it('"
+                LINE="LINE"
+                delit=${firstLine:0:2}
+                LINE1=${LINE:0:4}
+            if [[ $a == *"it("* ]]; then
+                if [ "$firstLine" == "it('LINE" -o "$firstLine" == 'it("LINE' ]; then
+                    numLineDelete=${arrString[1]}
+                    DE=$(sed "s/$numLineDelete/$numLine/g"<<<"${a}")
+                    echo $DE
+                else 
+                    insertNumber=$(sed "s/$itString/it('LINE $numLine - /g"<<<"${a}")
+                    echo $insertNumber
+                fi
+        
             elif [ "$firstLine" == "//" ]; then
                 echo $a
             else
             echo $a
             fi
     numLine=$((numLine+1))
-done < <(grep "" $path) > $path.t ; mv $path{.t,}
+done < <(grep "" $path)  > $path.t ; mv $path{.t,}
 done < <(find . -type f | sort -n)
